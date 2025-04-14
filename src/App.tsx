bcb64c38ab4633f503a04d8ea48387bac9578b9c
Mostak Khan <Mostak1993@gmail.com>
2025-04-14 18:05:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen bg-blue-300">
+      <div className="grid justify-center m-auto gap-5">
+        <h1 className="text-center">React JS + TailwindCSS Boilerplate</h1>
+        <p className="text-lg text-justify justify-center m-auto w-[480px]">
+          This is a boilerplate using Vite JS and Tailwind CSS It also has react
+          router dom installed which can be used to create navigation bars.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
